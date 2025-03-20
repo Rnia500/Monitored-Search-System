ec2529a82c67aee7975020d74da97c57a22bcb7b
Alan-scott34 <alanscott899@gmail.com>
2025-03-20 12:01:32 +0100

    home and search page

--- backend/database/models/baot.js
+++ backend/database/models/baot.js
@@ -3,10 +3,27 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../database/db');
 
 const Boat = sequelize.define('Boat', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  name: { type: DataTypes.STRING, allowNull: false },
-  type: { type: DataTypes.STRING, allowNull: false },
-  capacity: { type: DataTypes.INTEGER, allowNull: false }
+  id: { 
+    type: DataTypes.INTEGER, 
+    primaryKey: true, 
+    autoIncrement: true 
+  },
+
+  name: {
+     type: DataTypes.STRING, 
+     allowNull: false 
+    },
+    
+  type: { 
+    type: DataTypes.STRING,
+     allowNull: false 
+    },
+
+  capacity: {
+     type: DataTypes.INTEGER, 
+     allowNull: false 
+    }
+    
 }, {
   tableName: 'boats',
   timestamps: true
