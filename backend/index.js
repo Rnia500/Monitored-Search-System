6ba6046f9051d5f286a5b5bf580cf3a6c313c17f
Rnia500 <raniahamadoubachirou@gmail.com>
2025-03-26 10:30:37 +0100

    Testing the changes

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ app.set('views', path.join(__dirname, 'views'));
 // Serve static files (CSS, images, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use the code below to add the session validation to your server application.
+// More info: https://docs.descope.com/build/guides/session
+
+
 // Routes for different pages
 app.get('/', (req, res) => res.render('index'));
 app.get('/about', (req, res) => res.render('about'));
@@ -20,3 +24,4 @@ app.get('/register', (req, res) => res.render('register'));
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
